58e3ae6151cf558e1a6f71027078cc1b1d684e1a
AwePhD <mathias.reus@gipsa-lab.fr>
2024-02-14 17:47:45 +0100

    refactor(dataset): major refactor
    - Removing magic variables
    - Set constant values -> reduce argument
    - Format
    - more straight forward var names

--- scripts/dataGen.py
+++ scripts/dataGen.py
@@ -1,29 +1,58 @@
 import multiprocessing as mp
 from functools import partial
-import vitaldb as vdb
 import pandas as pd
-from typing import List
+import vitaldb as vdb
 from tqdm import tqdm
 
+PERCENT_MISSING_DATA_THRESHOLD = 0.4
+AGE_CASE_THRESHOLD = 18
+# Duration in seconds
+CASEEND_CASE_THRESHOLD = 3600
+FORBIDDEN_OPNAME_CASE = "transplant"
 
-def downloadOneCase(caseid: int,
-                    signal_list: List[str],
-                    static_data: List[str],
-                    sampling_time: float,
-                    df_cases: pd.DataFrame) -> pd.DataFrame:
-    """downloadOneCase function downloads the data from the vitaldb for a single case and returns the data in a pandas dataframe.
+TRACK_LIST_URL = "https://api.vitaldb.net/trks"
+CASE_INFO_URL = "https://api.vitaldb.net/cases"
+
+TRACK_NAME_SOLAR = "Solar8000/ART_MBP"
+
+STATIC_DATA_NAMES = ["age", "bmi", "asa", "preop_cr", "preop_htn", "opname"]
+
+SOLAR_DEVICE_NAME = "Solar8000"
+SOLAR_SIGNAL_NAMES = [
+    "ART_MBP",
+    "ART_SBP",
+    "ART_DBP",
+    "HR",
+    "RR",
+    "PLETH_SPO2",
+    "ETCO2",
+]
+ORCHESTRA_DEVICE_NAME = "Orchestra"
+ORCHESTRA_SIGNAL_NAMES = ["PPF20_CT"]
+PRIMUS_DEVICE_NAME = "Primus"
+PRIMUS_SIGNAL_NAMES = ["MAC"]
+
+# All in seconds
+SAMPLING_TIME = 1
+PRIMUS_SAMPLING_TIME = 7
+SOLAR_SAMPLING_TIME = 2
+
+
+def download_one_case(
+    caseid: int,
+    signal_names: list[str],
+    cases: pd.DataFrame,
+) -> pd.DataFrame | None:
+    """Downloads the data from the vitaldb for a single case and returns it.
+    Might return None if the data does not pass the filter.
 
     Parameters
     ----------
     caseid : int
         Case id to download.
-    signal_list : List[str]
-        signal list to download.
-    static_data : List[str]
-        static data to download.
-    sampling_time : float
-        sampling time for the signals (in seconds).
-    df_cases : pd.DataFrame
+    signal_names : list[str]
+        signal names to download.
+    cases : pd.DataFrame
         dataframe containing the cases information.
 
     Returns
@@ -31,95 +60,110 @@ def downloadOneCase(caseid: int,
     pd.DataFrame
         Returns the data in a pandas dataframe or None if empty.
     """
-    data_patient = vdb.VitalFile(caseid, signal_list)
-    data_patient = data_patient.to_pandas(signal_list, sampling_time)
-    data_patient.insert(0, 'caseid', caseid)
-    # check if there is enought data for each signal
-    flag_signal = False
-    for signal in signal_list:
-        if 'Solar8000' in signal:  # Solar8000 has 0.5Hz sampling rate
-            device_sampling_time = 2
-        elif 'Orchestra' in signal:  # ok if missing propofol data
-            continue
-        elif 'Primus' in signal:  # Primus has 1/7Hz sampling rate
-            device_sampling_time = 7
+    data_patient = vdb.VitalFile(caseid, signal_names)
+    data_patient = data_patient.to_pandas(signal_names, SAMPLING_TIME)
+    data_patient.insert(0, "caseid", caseid)
 
-        # if more than 40% of the data is missing, skip the case
-        if data_patient[signal].isna().sum() > len(data_patient) * (1 - sampling_time/device_sampling_time)*(1+0.4):
-            # print(f"Case {caseid} has more than 30% missing data for {signal}. Skipping case.")
+    # check if there is enough data for each signal
+    for signal in signal_names:
+        if SOLAR_DEVICE_NAME in signal:
+            device_sampling_time = SOLAR_SAMPLING_TIME
+        elif ORCHESTRA_DEVICE_NAME in signal:  # ok if missing propofol data
+            continue
+        elif PRIMUS_DEVICE_NAME in signal:
+            device_sampling_time = PRIMUS_SAMPLING_TIME
+        sampling_rate = SAMPLING_TIME / device_sampling_time
+
+        # if more than PERCENT_MISSING_DATA_THRESHOLD of the data is missing,
+        # -> skip the case
+        has_not_enough_data = data_patient[signal].isna().sum() > len(data_patient) * (
+            1 - sampling_rate
+        ) * (1 + PERCENT_MISSING_DATA_THRESHOLD)
+        if has_not_enough_data:
             return None
 
-    # download static data
-    data_patient_static = df_cases[df_cases['caseid'] == caseid][static_data]
-    data_patient_static['caseid'] = caseid
-    if data_patient_static.isna().sum().sum() > 0:  # if there is missing static data, skip the case
+    # get static data frome our case
+    data_patient_static = cases.query(f"caseid == {caseid}")[STATIC_DATA_NAMES]
+    data_patient_static["caseid"] = caseid
+
+    if data_patient_static.isna().any().any():
         return None
 
     # merge the data
-    data_patient = pd.merge(data_patient, data_patient_static, on='caseid')
-
+    data_patient = pd.merge(data_patient, data_patient_static, on="caseid")
     return data_patient
 
 
-def dataGen(sampling_time: float, signal_list: List[str], static_data: List[str]) -> pd.DataFrame:
-    """dataGEn function downloads the data from the vitaldb and returns the data in a pandas dataframe.
-    Columns of the output dataframe are the caseid + signals name +  static_data.
+def download_dataset(signal_names: list[str]) -> pd.DataFrame:
+    """Downloads the data from the vitaldb and returns the data in a pandas
+    dataframe.
 
     Parameters
     ----------
-    sampling_time : int
-        Sampling time of the downloaded data.
-    signal_list : List[str]
-        List of signals name to download.
-    static_data : List[str]
-        List of static data to download.
+    signal_names : list[str]
+        Signal names to download.
 
     Returns
     -------
     pd.DataFrame
-        Returns the data in a pandas dataframe.
+        Columns of the output dataframe are the caseid + signal names +
+        static_data.
     """
-    df_trks = pd.read_csv('https://api.vitaldb.net/trks')  # read track list
-    df_cases = pd.read_csv("https://api.vitaldb.net/cases")  # read case information
+    tracks = pd.read_csv(TRACK_LIST_URL)
+    cases = pd.read_csv(CASE_INFO_URL)
 
-    # select case list
+    # select case list which fit all those conditions
     caseids = list(
-        set(df_trks[df_trks['tname'] == 'Solar8000/ART_MBP']['caseid']) &  # select cases with arterial blood pressure
-        set(df_cases[df_cases['age'] > 18]['caseid']) &  # select cases with age > 18
-        set(df_cases[df_cases['caseend'] > 3600]['caseid']) &  # select cases with duration > 1 hour
-        set(df_cases[~df_cases['opname'].str.contains("transplant", case=False)]
-            ['caseid'])  # exclude cases with 'transplant' in operation name
+        # Track name is TRACK_NAME_SOLAR.
+        set(tracks.query(f"tname == '{TRACK_NAME_SOLAR}'").caseid)
+        &
+        # Case should have more than AGE_CASE_THRESHOLD age.
+        set(cases.query(f"age > {AGE_CASE_THRESHOLD}").caseid)
+        &
+        # Case should have duration > CASEEND_CASE_THRESHOLD.
+        set(cases.query(f"caseend > {CASEEND_CASE_THRESHOLD}").caseid)
+        &
+        # Case should NOT be have operator named .
+        set(cases[~cases.opname.str.contains(FORBIDDEN_OPNAME_CASE, case=False)].caseid)
     )
-
     print(f"Number of cases to consider: {len(caseids)}")
 
+    worker_download_function = partial(
+        download_one_case,
+        signal_names=signal_names,
+        cases=cases,
+    )
+
     # download the selected data
-    data = pd.DataFrame()
-
-    temp_function = partial(downloadOneCase, signal_list=signal_list, static_data=static_data,
-                            sampling_time=sampling_time, df_cases=df_cases)
     with mp.Pool(mp.cpu_count()) as pool:
-        data_patient = list(tqdm(pool.map(temp_function, caseids), total=len(caseids), desc="Downloading data"))
-    data_patient = [x for x in data_patient if x is not None]
-    data = pd.concat(data_patient)
+        cases_data = list(
+            tqdm(
+                pool.map(worker_download_function, caseids),
+                total=len(caseids),
+                desc="Downloading data",
+            )
+        )
+    dataset = pd.concat(
+        [case_data for case_data in cases_data if case_data is not None]
+    )
 
-    print(f"Number of cases with enough data: {len(data['caseid'].unique())}")
-    return data
+    print(f"Number of cases with enough data: {len(dataset.caseid.unique())}")
+    return dataset
+
+
+def main():
+    # signal names: <DEVICE_NAME>/<SIGNAL_NAME>
+    signal_names = (
+        [f"{SOLAR_DEVICE_NAME}/" + f for f in SOLAR_SIGNAL_NAMES]
+        + [f"{ORCHESTRA_DEVICE_NAME}/" + f for f in ORCHESTRA_SIGNAL_NAMES]
+        + [f"{PRIMUS_DEVICE_NAME}/" + f for f in PRIMUS_SIGNAL_NAMES]
+    )
+
+    dataset = download_dataset(signal_names)
+
+    print(dataset.head())
+    dataset.to_csv("data.csv", index=False)
 
 
 if __name__ == "__main__":
-    sampling_time = 1
-    # list of features
-    solar8000_signals = ['ART_MBP', 'ART_SBP', 'ART_DBP', 'HR', 'RR', 'PLETH_SPO2', 'ETCO2']
-    orchestra_signals = ['PPF20_CT']
-    primus_signals = ['MAC']
-    signals = ['Solar8000/'+f for f in solar8000_signals] + \
-        ['Orchestra/'+f for f in orchestra_signals] + \
-        ['Primus/'+f for f in primus_signals]
-
-    # list of static data
-    static = ['age', 'bmi', 'asa', 'preop_cr', 'preop_htn', 'opname']
-
-    dataframe = dataGen(sampling_time, signals, static)
-    print(dataframe.head())
-    dataframe.to_csv('data.csv', index=False)
+    main()
