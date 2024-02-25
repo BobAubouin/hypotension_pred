f5e7f3e32b3c55c46d01bc598b57d4fcf4ff24a8
Bob Aubouin <bob.aubouin@tutanota.com>
2024-02-25 21:38:27 +0100

    parralelize loader

--- scripts/dataLoader.py
+++ scripts/dataLoader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import concurrent.futures
 from tqdm import tqdm
 
 MIN_TIME_IOH = 60  # minimum time for the IOH to be considered as IOH (in seconds)
@@ -82,37 +83,142 @@ def validate_segment(segment: pd.DataFrame, previous_segment: pd.DataFrame, samp
         return False
 
     # test too much nan in the segment
-    # flag = False
-    # for signal in POSSIBLE_SIGNAL_NAME:
-    #     if signal == 'mac':
-    #         device_rate = DEVICE_NAME_TO_SAMPLING_RATE['Primus']
-    #     elif signal == 'pp_ct':
-    #         device_rate = 1
-    #     else:
-    #         device_rate = DEVICE_NAME_TO_SAMPLING_RATE['Solar8000']
+    flag = False
+    for signal in POSSIBLE_SIGNAL_NAME:
+        if signal == 'mac':
+            device_rate = DEVICE_NAME_TO_SAMPLING_RATE['Primus']
+        elif signal == 'pp_ct':
+            device_rate = 1
+        else:
+            device_rate = DEVICE_NAME_TO_SAMPLING_RATE['Solar8000']
 
-    #     nan_ratio = max(0, 1 - sampling_time/device_rate)
-    #     threshold = nan_ratio + (1 - nan_ratio)*MAX_NAN_SGEMENT
+        nan_ratio = max(0, 1 - sampling_time/device_rate)
+        threshold = nan_ratio + (1 - nan_ratio)*MAX_NAN_SGEMENT
 
-    #     if segment[signal].isna().sum() > threshold*segment_length:
-    #         flag = True
-    #         break
-    # if flag:
-    #     return False
+        if segment[signal].isna().sum() > threshold*segment_length:
+            flag = True
+            break
+    if flag:
+        return False
 
     return True
 
 
-def dataLoader(half_times: list[int] = [10, 60, 5*60],
-               signal_name: list[str] = ['mbp'],
-               static_data: list[str] = [],
-               leading_time: int = 3*60,
-               prediction_windows: int = 7*60,
-               observation_windows: int = 5*60,
-               sampling_time: int = 2,
-               window_shift: int = 30,
-               max_number_of_case: int = 5000
-               ):
+def process_cases(
+        df_case: pd.DataFrame,
+        sampling_time: int,
+        observation_windows: int,
+        leading_time: int,
+        prediction_windows: int,
+        window_shift: int,
+        half_times: list[int],
+        signal_name: list[str],
+        static_data: list[str],
+        caseid_list: list[list[str]],
+        pbar: tqdm = None):
+    """process_cases function process the cases to be used in the model.
+
+    Parameters
+    ----------
+    df_case : pd.DataFrame
+        The dataframe containing the case data.
+    sampling_time : int
+        The sampling time in minutes.
+    observation_windows : int
+        The length of the observation window in seconds.
+    leading_time : int
+        The length of the leading time in seconds.
+    prediction_windows : int
+        The length of the prediction window in seconds.
+    window_shift : int
+        The shift between the segments in seconds.
+    half_times : list[int]
+        List of half-time to use in exponential moving average and variance (seconds).
+    signal_name : list[str]
+        List of signals name to consider.
+    static_data : list[str]
+        List of static data to consider.
+    caseid_list : list[list[str]]
+        List of caseid to consider for cross validation.
+    pbar : tqdm
+        The progress bar to update.
+
+    Returns
+    -------
+    pd.DataFrame
+        Returns the data in a pandas dataframe.
+    """
+    formattedData_case = pd.DataFrame()
+    number_of_raw_segment = 0
+    number_of_selected_segement = 0
+    # process mean arterial value data
+    # remove too low value (before the start of the measurement)
+    df_case.mbp.mask(df_case.mbp < MIN_MPB_SGEMENT, inplace=True)
+    # removing the nan values at the beginning and the ending
+    case_valid_mask = ~df_case.mbp.isna()
+    df_case = df_case[(np.cumsum(case_valid_mask) > 0) & (np.cumsum(case_valid_mask[::-1])[::-1] > 0)]
+
+    # create label
+    df_case = label_caseid(df_case, sampling_time)
+
+    # create time series
+    time = np.arange(df_case.shape[0])*sampling_time
+    df_case.insert(0, 'time', time)
+
+    # create the segments
+    segment_length = (observation_windows + leading_time + prediction_windows)//sampling_time
+    segment_shift = window_shift // sampling_time
+
+    for time_start in range(0, df_case.shape[0] - segment_length, segment_shift):
+        segment = df_case.iloc[time_start:time_start + segment_length]
+        previous_segment = df_case.iloc[max(0, time_start - RECOVERY_TIME//sampling_time):time_start]
+        number_of_raw_segment += 1
+        if not validate_segment(segment, previous_segment, sampling_time, observation_windows, leading_time):
+            continue
+
+        number_of_selected_segement += 1
+        segment_obs = segment.iloc[:observation_windows//sampling_time]
+        segment_pred = segment.iloc[(observation_windows + leading_time)//sampling_time:]
+        # create the time series features
+        segment_data = pd.DataFrame()
+        for half_time in half_times:
+            for signal in signal_name:
+                segment_data[f'{signal}_ema_{half_time}'] = [segment_obs[signal].ewm(
+                    halflife=half_time).mean().iloc[-1]]
+                segment_data[f'{signal}_var_{half_time}'] = [segment_obs[signal].ewm(
+                    halflife=half_time).std().iloc[-1]]
+        # add the label of the segment
+        segment_data['label'] = [(segment_pred.label.sum() > 0).astype(int)]
+
+        # add the segment to the data
+        formattedData_case = pd.concat([formattedData_case, segment_data])
+
+    # add the static features
+    for static in static_data:
+        formattedData_case[static] = df_case[static].iloc[0]
+
+    # add the caseid and the group of the caseid
+    formattedData_case['caseid'] = df_case['caseid'].iloc[0]
+    # find in which group the caseid is
+    for i, group in enumerate(caseid_list):
+        if df_case['caseid'].iloc[0] in group:
+            formattedData_case['cv_group'] = i
+            break
+    if pbar is not None:
+        pbar.update(1)
+    return formattedData_case, number_of_raw_segment, number_of_selected_segement
+
+
+def dataLoaderParallel(half_times: list[int] = [10, 60, 5*60],
+                       signal_name: list[str] = ['mbp'],
+                       static_data: list[str] = [],
+                       leading_time: int = 3*60,
+                       prediction_windows: int = 7*60,
+                       observation_windows: int = 5*60,
+                       sampling_time: int = 2,
+                       window_shift: int = 30,
+                       max_number_of_case: int = 5000
+                       ):
     """dataLoader function loads data genrated by dataGen function and process the data to be used in the model.
 
     Parameters
@@ -166,73 +272,36 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
     number_of_selected_segement = 0
 
     # get all caseid and separate them in 5 groups
-    caseid_list = rawData['caseid'].unique()
+    caseid_list_raw = rawData['caseid'].unique()
     np.random.seed(0)
-    np.random.shuffle(caseid_list)
-    caseid_list = np.array_split(caseid_list, NUMBER_CV_FOLD)
-    pbar = tqdm(total=len(rawData['caseid'].unique()), desc='Processing caseid')
-    j = 0
-    for caseid, df_case in rawData.groupby('caseid'):
-        j += 1
-        if j > max_number_of_case:
-            break
-        pbar.update(1)
-        formattedData_case = pd.DataFrame()
-        # process mean arterial value data
-        # remove too low value (before the start of the measurement)
-        df_case.mbp.mask(df_case.mbp < MIN_MPB_SGEMENT, inplace=True)
-        # removing the nan values at the beginning and the ending
-        case_valid_mask = ~df_case.mbp.isna()
-        df_case = df_case[(np.cumsum(case_valid_mask) > 0) & (np.cumsum(case_valid_mask[::-1])[::-1] > 0)]
+    np.random.shuffle(caseid_list_raw)
+    caseid_list = np.array_split(caseid_list_raw, NUMBER_CV_FOLD)
 
-        # create label
-        df_case = label_caseid(df_case, sampling_time)
+    pbar = tqdm(total=min(max_number_of_case, len(rawData['caseid'].unique())), desc='Processing caseid')
 
-        # create time series
-        time = np.arange(df_case.shape[0])*sampling_time
-        df_case.insert(0, 'time', time)
+    rawData = rawData[rawData['caseid'].isin(caseid_list_raw[:max_number_of_case])]
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = {executor.submit(process_cases,
+                                   df_case,
+                                   sampling_time,
+                                   observation_windows,
+                                   leading_time,
+                                   prediction_windows,
+                                   window_shift,
+                                   half_times,
+                                   signal_name,
+                                   static_data,
+                                   caseid_list
+                                   ): caseid for caseid, df_case in rawData.groupby('caseid')}
 
-        # create the segments
-        segment_length = (observation_windows + leading_time + prediction_windows)//sampling_time
-        segment_shift = window_shift // sampling_time
+        for future in concurrent.futures.as_completed(futures):
+            caseid = futures[future]
+            formattedData_case, number_of_raw_segment_case, number_of_selected_segement_case = future.result()
+            formattedData = pd.concat([formattedData, formattedData_case])
+            number_of_raw_segment += number_of_raw_segment_case
+            number_of_selected_segement += number_of_selected_segement_case
+            pbar.update(1)
 
-        for time_start in range(0, df_case.shape[0] - segment_length, segment_shift):
-            segment = df_case.iloc[time_start:time_start + segment_length]
-            previous_segment = df_case.iloc[max(0, time_start - RECOVERY_TIME//sampling_time):time_start]
-            number_of_raw_segment += 1
-            if not validate_segment(segment, previous_segment, sampling_time, observation_windows, leading_time):
-                continue
-
-            number_of_selected_segement += 1
-            segment_obs = segment.iloc[:observation_windows//sampling_time]
-            segment_pred = segment.iloc[(observation_windows + leading_time)//sampling_time:]
-            # create the time series features
-            segment_data = pd.DataFrame()
-            for half_time in half_times:
-                for signal in signal_name:
-                    segment_data[f'{signal}_ema_{half_time}'] = [segment_obs[signal].ewm(
-                        halflife=half_time).mean().iloc[-1]]
-                    segment_data[f'{signal}_var_{half_time}'] = [segment_obs[signal].ewm(
-                        halflife=half_time).std().iloc[-1]]
-            # add the label of the segment
-            segment_data['label'] = [(segment_pred.label.sum() > 0).astype(int)]
-
-            # add the segment to the data
-            formattedData_case = pd.concat([formattedData_case, segment_data])
-
-        # add the static features
-        for static in static_data:
-            formattedData_case[static] = df_case[static].iloc[0]
-
-        # add the caseid and the group of the caseid
-        formattedData_case['caseid'] = df_case['caseid'].iloc[0]
-        # find in which group the caseid is
-        for i, group in enumerate(caseid_list):
-            if df_case['caseid'].iloc[0] in group:
-                formattedData_case['cv_group'] = i
-                break
-
-        formattedData = pd.concat([formattedData, formattedData_case])
     pbar.close()
     print(f'Number of raw segment: {number_of_raw_segment}')
     print(f'Number of selected segment: {number_of_selected_segement}')
@@ -240,9 +309,9 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
 
 
 if __name__ == "__main__":
-    dataframe = dataLoader(
+    dataframe = dataLoaderParallel(
         half_times=[10],
-        max_number_of_case=500
+        max_number_of_case=5000
     )
 
     print(f"Number of ioh: {dataframe.label.sum()}")
