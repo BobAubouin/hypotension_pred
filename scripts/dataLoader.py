704252d50fe9ad8c9f833ec44c140a6691b35314
Bob Aubouin <bob.aubouin@tutanota.com>
2024-02-25 11:51:54 +0100

    baseline working

--- scripts/dataLoader.py
+++ scripts/dataLoader.py
@@ -6,9 +6,10 @@ MIN_TIME_IOH = 60  # minimum time for the IOH to be considered as IOH (in second
 MIN_VALUE_IOH = 65  # minimum value for the mean arterial pressure to be considered as IOH (in mmHg)
 MIN_MPB_SGEMENT = 40  # minimum acceptable value for the mean arterial pressure (in mmHg)
 MAX_MPB_SGEMENT = 150  # maximum acceptable value for the mean arterial pressure (in mmHg)
-MAX_DELTA_MPB_SGEMENT = 30  # maximum acceptable value for the mean arterial pressure variation (in mmHg)
+MAX_DELTA_MPB_SGEMENT = 30  # maximum acceptable value for the mean arterial pressure variation (in mmHg/minutes)
 MAX_NAN_SGEMENT = 0.1  # maximum acceptable value for the nan in the segment (in %)
 NUMBER_CV_FOLD = 5  # number of cross-validation fold
+RECOVERY_TIME = 15*60  # recovery time after the IOH (in seconds)
 
 DEVICE_NAME_TO_SAMPLING_RATE = {
     "Solar8000": 2,
@@ -17,13 +18,34 @@ DEVICE_NAME_TO_SAMPLING_RATE = {
 POSSIBLE_SIGNAL_NAME = ['mbp', 'sbp', 'dbp', 'hr', 'rr', 'spo2', 'etco2', 'mac', 'pp_ct']
 
 
-def validate_segment(segment: pd.DataFrame, sampling_time: int, observation_windows: int, leading_time: int):
+def label_caseid(df_case: pd.DataFrame, sampling_time: int):
+    """
+    Labels the case based on the mean arterial blood pressure (mbp) values.
+
+    Args:
+        df_case (pd.DataFrame): The dataframe containing the case data.
+        sampling_time (int): The sampling time in minutes.
+
+    Returns:
+        pd.DataFrame: The dataframe with the label column added.
+    """
+    # create the label for the case
+    label = (np.convolve((df_case['mbp'] < MIN_VALUE_IOH).astype(int), np.ones(
+        MIN_TIME_IOH//sampling_time), mode='valid') == MIN_TIME_IOH//sampling_time).astype(int)
+    # complete the label to have the same length as the data
+    label = np.concatenate([np.zeros(df_case.shape[0] - label.shape[0]), label])
+    # add label to the data
+    df_case.insert(0, 'label', label)
+    return df_case
+
+
+def validate_segment(segment: pd.DataFrame, previous_segment: pd.DataFrame, sampling_time: int, observation_windows: int, leading_time: int):
     """ Validate_segment function validate the segment to be used in the model.
 
-    The cionditions to validate the segment are:
+    The conditions to validate the segment are:
     - The mean arterial pressure (mbp) is between 40 and 150 mmHg.
     - The variation of the mean arterial pressure (mbp) is less than 30 mmHg/minutes.
-    - The label is not in the observation window.
+    - The label is not in the observation window not in the recovery time.
     - The nan values are less than 10% of the segment.
 
     Parameters
@@ -50,11 +72,15 @@ def validate_segment(segment: pd.DataFrame, sampling_time: int, observation_wind
     if (segment.mbp > MAX_MPB_SGEMENT).any():
         return False
     # test any delta MAP > 30mmHg/minutes
-    if (segment.mbp.max() - segment.mbp.min()) > MAX_DELTA_MPB_SGEMENT:
+    if (segment.mbp.diff().rolling(window=60//sampling_time).mean().max()) > MAX_DELTA_MPB_SGEMENT:
         return False
     # test any label in the observation window
     if segment.label[:(observation_windows+leading_time)//sampling_time].any():
         return False
+    # test if any label in the previous segment
+    if previous_segment.label.sum() > 0:
+        return False
+
     # test too much nan in the segment
     # flag = False
     # for signal in POSSIBLE_SIGNAL_NAME:
@@ -85,6 +111,7 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
                observation_windows: int = 5*60,
                sampling_time: int = 2,
                window_shift: int = 30,
+               max_number_of_case: int = 5000
                ):
     """dataLoader function loads data genrated by dataGen function and process the data to be used in the model.
 
@@ -117,6 +144,7 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
             raise ValueError(f'{signal} is not a possible signal name')
 
     # Load the data
+    print('Loading raw data...')
     rawData = pd.read_csv(f'data/data_async.csv')
     # rename the raw to have simpler names
     rawData.rename(columns={'Solar8000/ART_MBP': 'mbp',
@@ -142,11 +170,11 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
     np.random.seed(0)
     np.random.shuffle(caseid_list)
     caseid_list = np.array_split(caseid_list, NUMBER_CV_FOLD)
-    pbar = tqdm(total=len(caseid_list), desc='Processing caseid')
+    pbar = tqdm(total=len(rawData['caseid'].unique()), desc='Processing caseid')
     j = 0
     for caseid, df_case in rawData.groupby('caseid'):
         j += 1
-        if j > 100:
+        if j > max_number_of_case:
             break
         pbar.update(1)
         formattedData_case = pd.DataFrame()
@@ -158,12 +186,8 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
         df_case = df_case[(np.cumsum(case_valid_mask) > 0) & (np.cumsum(case_valid_mask[::-1])[::-1] > 0)]
 
         # create label
-        label = (np.convolve((df_case['mbp'] < MIN_VALUE_IOH).astype(int), np.ones(
-            MIN_TIME_IOH), mode='valid') == MIN_TIME_IOH).astype(int)
-        # complete the label to have the same length as the data
-        label = np.concatenate([np.zeros(df_case.shape[0] - label.shape[0]), label])
-        # add label to the data
-        df_case.insert(0, 'label', label)
+        df_case = label_caseid(df_case, sampling_time)
+
         # create time series
         time = np.arange(df_case.shape[0])*sampling_time
         df_case.insert(0, 'time', time)
@@ -174,23 +198,24 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
 
         for time_start in range(0, df_case.shape[0] - segment_length, segment_shift):
             segment = df_case.iloc[time_start:time_start + segment_length]
+            previous_segment = df_case.iloc[max(0, time_start - RECOVERY_TIME//sampling_time):time_start]
             number_of_raw_segment += 1
-            if not validate_segment(segment, sampling_time, observation_windows, leading_time):
+            if not validate_segment(segment, previous_segment, sampling_time, observation_windows, leading_time):
                 continue
 
             number_of_selected_segement += 1
-
+            segment_obs = segment.iloc[:observation_windows//sampling_time]
+            segment_pred = segment.iloc[(observation_windows + leading_time)//sampling_time:]
             # create the time series features
             segment_data = pd.DataFrame()
             for half_time in half_times:
                 for signal in signal_name:
-                    segment_data[f'{signal}_ema_{half_time}'] = [segment[signal].ewm(
+                    segment_data[f'{signal}_ema_{half_time}'] = [segment_obs[signal].ewm(
                         halflife=half_time).mean().iloc[-1]]
-                    segment_data[f'{signal}_var_{half_time}'] = [segment[signal].ewm(
+                    segment_data[f'{signal}_var_{half_time}'] = [segment_obs[signal].ewm(
                         halflife=half_time).std().iloc[-1]]
             # add the label of the segment
-            segment_data['label'] = [(segment.label.iloc[(observation_windows + leading_time) //
-                                     sampling_time:].sum() > 0).astype(int)]
+            segment_data['label'] = [(segment_pred.label.sum() > 0).astype(int)]
 
             # add the segment to the data
             formattedData_case = pd.concat([formattedData_case, segment_data])
@@ -214,7 +239,11 @@ def dataLoader(half_times: list[int] = [10, 60, 5*60],
     return formattedData
 
 
-dataframe = dataLoader(
-    half_times=[10],
-)
-dataframe.to_csv('data/data_baseline.csv', index=False)
+if __name__ == "__main__":
+    dataframe = dataLoader(
+        half_times=[10],
+        max_number_of_case=500
+    )
+
+    print(f"Number of ioh: {dataframe.label.sum()}")
+    dataframe.to_csv('data/data_baseline.csv', index=False)
