81cf1bb47ac6ea6ebdd15eac9b2d35a31f76df90
Bob Aubouin <bob.aubouin@tutanota.com>
2024-02-27 20:35:27 +0100

    fix loader and run model again

--- scripts/dataLoader.py
+++ scripts/dataLoader.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
-import concurrent.futures
+import multiprocessing as mp
+from functools import partial
 from tqdm import tqdm
+import concurrent.futures
 
 WINDOW_SIZE_PEAK = 500  # window size for the peak detection
 TRESHOLD_PEAK = 30  # threshold for the peak detection
@@ -10,7 +12,7 @@ MIN_VALUE_IOH = 65  # minimum value for the mean arterial pressure to be conside
 MIN_MPB_SGEMENT = 40  # minimum acceptable value for the mean arterial pressure (in mmHg)
 MAX_MPB_SGEMENT = 150  # maximum acceptable value for the mean arterial pressure (in mmHg)
 MAX_DELTA_MPB_SGEMENT = 30  # maximum acceptable value for the mean arterial pressure variation (in mmHg/minutes)
-MAX_NAN_SGEMENT = 0.1  # maximum acceptable value for the nan in the segment (in %)
+MAX_NAN_SGEMENT = 0.2  # maximum acceptable value for the nan in the segment (in %)
 NUMBER_CV_FOLD = 5  # number of cross-validation fold
 RECOVERY_TIME = 10*60  # recovery time after the IOH (in seconds)
 
@@ -44,7 +46,7 @@ def preprocess_bpdata(df_case: pd.DataFrame, sampling_time: int):
     df_case.mbp.mask(df_case.mbp < MIN_MPB_SGEMENT, inplace=True)
     # removing the nan values at the beginning and the ending
     case_valid_mask = ~df_case.mbp.isna()
-    df_case = df_case[(np.cumsum(case_valid_mask) > 0) & (np.cumsum(case_valid_mask[::-1])[::-1] > 0)]
+    df_case = df_case[(np.cumsum(case_valid_mask) > 0) & (np.cumsum(case_valid_mask[::-1])[::-1] > 0)].copy()
 
     # remove peaks on the mean arterial pressure
 
@@ -53,13 +55,9 @@ def preprocess_bpdata(df_case: pd.DataFrame, sampling_time: int):
     rolling_mean_dbp = df_case.dbp.rolling(window=WINDOW_SIZE_PEAK//sampling_time, center=True, min_periods=10).mean()
 
     # Identify peaks based on the difference from the rolling mean
-    mbp_peaks = df_case[(df_case.mbp-rolling_mean_mbp).abs() > TRESHOLD_PEAK]
-    df_case.loc[mbp_peaks.index, ['mbp']] = np.nan
-    sbp_peaks = df_case[(df_case.sbp-rolling_mean_sbp).abs() > TRESHOLD_PEAK*1.5]
-    df_case.loc[sbp_peaks.index, ['sbp']] = np.nan
-    dbp_peaks = df_case[(df_case.dbp - rolling_mean_dbp).abs() > TRESHOLD_PEAK]
-    df_case.loc[dbp_peaks.index, ['dbp']] = np.nan
-
+    df_case.mbp.mask((df_case.mbp-rolling_mean_mbp).abs() > TRESHOLD_PEAK)
+    df_case.sbp.mask((df_case.sbp-rolling_mean_sbp).abs() > TRESHOLD_PEAK*1.5)
+    df_case.dbp.mask((df_case.dbp-rolling_mean_dbp).abs() > TRESHOLD_PEAK)
     return df_case
 
 
@@ -80,12 +78,15 @@ def label_caseid(df_case: pd.DataFrame, sampling_time: int):
         The dataframe with the label column added.
     """
     # create the label for the case
-    label = (np.convolve((df_case['mbp'] < MIN_VALUE_IOH).astype(int), np.ones(
-        MIN_TIME_IOH//sampling_time), mode='valid') == MIN_TIME_IOH//sampling_time).astype(int)
-    # complete the label to have the same length as the data
-    label = np.concatenate([np.zeros(df_case.shape[0] - label.shape[0]), label])
+    label = df_case.mbp.rolling(MIN_TIME_IOH//sampling_time,
+                                min_periods=1).apply(lambda x: (x < MIN_VALUE_IOH).loc[~np.isnan(x)].all())
+    label.fillna(0, inplace=True)
+    label = (label + label.shift(-MIN_TIME_IOH//sampling_time, fill_value=0)).astype(bool).astype(int)
+    # if label pass from 1 to 0, we keep the 1 for the next MIN_TIME_IOH
+
     # add label to the data
-    df_case.insert(0, 'label', label)
+    df_case.insert(0, 'label', label.astype(int))
+
     return df_case
 
 
@@ -327,7 +328,6 @@ def dataLoaderParallel(half_times: list[int] = [10, 60, 5*60],
     np.random.seed(0)
     np.random.shuffle(caseid_list_raw)
     caseid_list = np.array_split(caseid_list_raw, NUMBER_CV_FOLD)
-
     pbar = tqdm(total=min(max_number_of_case, len(rawData['caseid'].unique())), desc='Processing caseid')
 
     rawData = rawData[rawData['caseid'].isin(rawData['caseid'].unique()[:max_number_of_case])]
@@ -354,6 +354,29 @@ def dataLoaderParallel(half_times: list[int] = [10, 60, 5*60],
             pbar.update(1)
 
     pbar.close()
+    # rawData = rawData[rawData['caseid'].isin(rawData['caseid'].unique()[:max_number_of_case])]
+
+    # process_cases_partial = partial(process_cases,
+    #                                 sampling_time=sampling_time,
+    #                                 observation_windows=observation_windows,
+    #                                 leading_time=leading_time,
+    #                                 prediction_windows=prediction_windows,
+    #                                 window_shift=window_shift,
+    #                                 half_times=half_times,
+    #                                 signal_name=signal_name,
+    #                                 static_data=static_data,
+    #                                 caseid_list=caseid_list
+    #                                 )
+
+    # with mp.Pool(mp.cpu_count()) as pool:
+    #     results = list(tqdm(pool.map(process_cases_partial, [df_case for _, df_case in rawData.groupby(
+    #         'caseid')]), total=len(rawData['caseid'].unique()), desc='Processing cases'))
+
+    # for formattedData_case, number_of_raw_segment_case, number_of_selected_segement_case in results:
+    #     formattedData = pd.concat([formattedData, formattedData_case])
+    #     number_of_raw_segment += number_of_raw_segment_case
+    #     number_of_selected_segement += number_of_selected_segement_case
+
     print(f'Number of raw segment: {number_of_raw_segment}')
     print(f'Number of selected segment: {number_of_selected_segement}')
     return formattedData
