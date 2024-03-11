1e567f7e00f242eac3ac810cb812e6d4dff359d1
Bob Aubouin <bob.aubouin@tutanota.com>
2024-03-11 16:02:39 +0100

    minor changes

--- tests/test_label.py
+++ tests/test_label.py
@@ -5,7 +5,7 @@ from scripts.dataLoader import label_caseid
 
 # Sample data for testing
 mbp = np.ones(200)*100
-mbp[45:76] = 64  # must be labeled as 1
+mbp[35:76] = 64  # must be labeled as 1
 mbp[110:140] = 55  # must be labeled as 1
 mbp[120:125] = np.nan  # despite the presence of nan
 mbp[150:180] = np.nan  # must be labeled as 0
@@ -20,14 +20,23 @@ def test_label_caseid():
     # Apply the function to the sample data
     labeled_df = label_caseid(df_sample.copy(), sampling_time)
 
+    # plot the reuslts
+
     # Check if the 'label' column is added
     assert 'label' in labeled_df.columns
 
     # Check the correctness of the labels based on the provided sample data
     expected_labels = np.zeros(200)
-    expected_labels[45:76] = 1
+    expected_labels[35:76] = 1
     expected_labels[110:140] = 1
-
+    # import matplotlib.pyplot as plt
+    # plt.plot(expected_labels)
+    # plt.plot(labeled_df['label'])
+    # plt.plot(mbp/100)
+    # label_id = labeled_df.label.diff().clip(lower=0).cumsum().fillna(0)
+    # label_id[labeled_df.label == 0] = np.nan
+    # plt.plot(label_id)
+    # plt.show()
     assert all(labeled_df['label'] == expected_labels)
 
     # Add more test cases if needed
